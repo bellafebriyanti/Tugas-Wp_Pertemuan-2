2746216abc3ed245fb7b343930bf7110d2e121a5
bellafebriyanti <91323261+bellafebriyanti@users.noreply.github.com>
2021-10-28 19:34:26 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+{
+    echo "Perkenalkan<br>";
+    echo "Nama Saya Bella Febriyanti<br>";
+    echo "Kelas 19.3B.05 dengan NIM 19201010";
+}
+}
